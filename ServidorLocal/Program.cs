2c97805460a78d330a5f7ad021a88b3f646a03eb
leosarturi <sarturilazier@gmail.com>
2025-10-17 22:04:00 -0300

    tirar lixos

--- ServidorLocal/Program.cs
+++ ServidorLocal/Program.cs
@@ -246,7 +246,7 @@ namespace ServidorLocal
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.WebHost.UseUrls("http://0.0.0.0:443");
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
             var app = builder.Build();
             app.UseWebSockets();
 
